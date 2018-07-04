0cfeac81445f434f97c81e21cf7ac10d34867f11
Pete Turnbull <pete.j.turnbull@gmail.com>
2018-07-04 19:26:44 +0100

    fix error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
-const Id = require('@hotelflex/id')
+const os = require('os')
+const df = require('node-df')
+const path = require('path')
+const appRootDir = require('app-root-path')
+const pjson = require(appRootDir + path.sep + 'package.json')
 
-module.exports = () => (ctx, next) => {
-  ctx.request.meta = {
-    transactionId: ctx.headers['transaction-id'] || Id.create(),
-    operationId: ctx.headers['operation-id'] || Id.create(),
+const DEFAULT_PATH = '/ping'
+
+const getInfo = () =>
+  new Promise(resolve => {
+    df((err, data) => {
+      if (err) return resolve({ msg: 'Error fetching disk info', error: err })
+      return resolve({
+        timestamp: Date.now(),
+        uptime: process.uptime(),
+        application: {
+          name: pjson.name,
+          version: pjson.version,
+          pid: process.pid,
+          title: process.title,
+          argv: process.argv,
+          versions: process.versions,
+          node_env: process.env.NODE_ENV,
+        },
+        resources: {
+          memory: process.memoryUsage(),
+          loadavg: os.loadavg(),
+          cpu: os.cpus(),
+          disk: data,
+          nics: os.networkInterfaces(),
+        },
+        system: {
+          arch: process.arch,
+          platform: process.platform,
+          type: os.type(),
+          release: os.release(),
+          hostname: os.hostname(),
+          uptime: os.uptime(),
+          cores: os.cpus().length,
+          memory: os.totalmem(),
+        },
+      })
+    })
+  })
+
+module.exports = p => async (ctx, next) => {
+  p = p || DEFAULT_PATH
+  if (p === ctx.path) {
+    ctx.body = await getInfo()
+  } else {
+    return next()
   }
-  return next()
 }
